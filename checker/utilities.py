a0c35f2ea582c83634a99122c5c3f8c1693d8ab5
Oudai-Gadhi <ogadhi5@gmail.com>
2025-11-28 14:45:27 -0800

    fix

--- checker/utilities.py
+++ checker/utilities.py
@@ -1,13 +1,16 @@
-def classify (score):
+# checker/utilities.py
+
+def classify(score):
+    score = float(score)
     if score >= 9.0:
-        return f"\033[{91}mCRITICAL\033[0m"
+        return "\033[91mCRITICAL\033[0m"  # Red
     elif score >= 7.0:
-        return f"\033[{93}mHIGH\033[0m"
+        return "\033[93mHIGH\033[0m"      # Yellow
     elif score >= 4.0:
-        return f"\033[{33}mMEDIUM\033[0m"
+        return "\033[33mMEDIUM\033[0m"    # Orange/Brown
     elif score >= 0.1:
-        return f"\033[{32}mLOW\033[0m"
+        return "\033[92mLOW\033[0m"       # Green
     else:
-        return f"\033[{25}mNONE\033[0m"
+        return "\033[90mNONE\033[0m"      # Gray
 
 
